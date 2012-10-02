1c465fc8e967f04e2d35f07767a85618183b3825
Eric Kiel <eric.kiel@collideoscope.org>
2012-10-02 15:57:34 -0500

    adding hotadd-status script

---
+++ hotadd-status.ps1
@@ -0,0 +1,31 @@
+# ==============================================================================================
+# NAME: hotadd-status.ps1
+# AUTHOR: Eric Kiel eric<dot>kiel<at>collideoscope<dot>org
+# DATE  : 10/02/2012
+# COMMENT: This script will connect to a VMware Virtual Center server and get VM status on 
+# CPUAddEnagbled, CPUHotRemoveEnabled, and MemoryHotAddEnabled.
+# VERSION: 1.0 - Original Code
+# USAGE: .\hotadd-status.ps1
+# Variables: 
+# $date - date in Month/Day/year format
+# $vcenter - vCenter host name
+# REQUIREMENTS: VMware vSphere PowerCLI
+# ==============================================================================================
+
+# Comment out if hard coding a vCenter below
+$vcenter = Read-Host "Please enter the the vCenter you would like to connect to"
+
+# Uncomment to hard code a vCenter server in
+#$vcenter = "your-vcenter"
+
+# Connect to vCenter and get the date...
+connect-viserver $vcenter
+$date = get-date -format MM-dd-yyyy
+
+# Get VM status on CPUAddEnagbled, CPUHotRemoveEnabled, and MemoryHotAddEnabled
+Get-VM | Get-View | Select Name, `
+@{N="CpuHotAddEnabled";E={$_.Config.CpuHotAddEnabled}}, `
+@{N="CpuHotRemoveEnabled";E={$_.Config.CpuHotRemoveEnabled}}, `
+@{N="MemoryHotAddEnabled";E={$_.Config.MemoryHotAddEnabled}} | `
+Export-Csv -NoTypeInformation $date-$vcenter-hot-add_status.csv
+write-host "Your report has been saved as" $date-$vcenter-hot-add_status.csv
