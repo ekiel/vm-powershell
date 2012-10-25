7ab190116fec52c265ce96174b6dcb85188f266c
Eric Kiel <eric.kiel@collideoscope.org>
2012-10-25 12:51:18 -0500

    VERSION: 1.4 - corrected isolation.device.connectable.disable - I had mistaken it for isolation.tools.connectable.disable which is not a valid option.

--- vsphere5_hardening.ps1
+++ vsphere5_hardening.ps1
@@ -1,12 +1,14 @@
 # ==============================================================================================
 # NAME: vsphere5_hardening.ps1
 # AUTHOR: Eric Kiel eric<dot>kiel<at>collideoscope<dot>org
-# DATE  : 10/17/2012
+# DATE  : 10/25/2012
 # COMMENT: This script will modify .vmx files to add parameters to increase VM security. Please
 # read and understand the security guide found at
 # http://www.vmware.com/support/support-resources/hardening-guides.html
-# VERSION: 1.3 - removed "vm targeting" -it was flaky and kinda scary, I'll work on this in the
-# next version. I've also updated to add every option mentioned in the hardening guide.
+# Please note that the VM must be shutdown and powered back on for additions to VMX file to take
+# effect.
+# VERSION: 1.4 - corrected isolation.device.connectable.disable - I had mistaken it for 
+# isolation.tools.connectable.disable which is not a valid option. Happy hardening!
 # USAGE: .\vsphere5_hardening.ps1
 # REQUIREMENTS: VMware vSphere PowerCLI
 # ==============================================================================================
@@ -20,12 +22,12 @@ $ExtraOptions = @{
 	# Disable virtual disk shrinking
 	"isolation.tools.diskShrink.disable"="true";
 	"isolation.tools.diskWiper.disable"="true";
-	
+
 	# 5.0 Prevent device removal-connection-modification of devices
 	"isolation.tools.setOption.disable"="true";
-	"isolation.tools.connectable.disable"="true";
+	"isolation.device.connectable.disable"="true";
 	"isolation.device.edit.disable"="true";
-		
+
 	# Disable copy/paste operations to/from VM
 	"isolation.tools.copy.disable"="true";
 	"isolation.tools.paste.disable"="true";
@@ -34,36 +36,36 @@ $ExtraOptions = @{
 
 	# Disable VMCI
 	"vmci0.unrestricted"="false";
-	
+
 	# Log Management
 	"tools.setInfo.sizeLimit"="1048576";
 	"log.keepOld"="10";
 	"log.rotateSize"="100000";
-	
+
 	# Limit console connections - choose how many consoles are allowed
-	#"RemoteDisplay.maxConnections"="1";
-	"RemoteDisplay.maxConnections"="2";
+	"RemoteDisplay.maxConnections"="1";
+	#"RemoteDisplay.maxConnections"="2";
 
 	# Remove floppy device - hardware needs to be removed by some other method
 	#"floppy0.present"="false";
-	
+
 	# Disable network boot option - not in hardening guide (no real fix for e1000; see 
 	# http://virtualfoundry.blogspot.com/2009/05/secrets-of-e1000.html	
 	"vlance.noOprom"="true";
 	"vmxnet.noOprom"="true";
-	
+
 	# 5.0 Disable serial port
 	"serial0.present"="false";
-	
+
 	# 5.0 Disable parallel port
 	"parallel0.present"="false";
-	
+
 	# 5.0 Disable USB
 	"usb.present"="false";
-	
+
 	# Disable CD/DVD Drive - hardware needs to be removed by some other method
 	#"ide1:0.present"="true";
-	
+
 	# 5.0 Disable certain unexposed features [Fusion, Workstation]
 	"isolation.tools.unity.push.update.disable"="true";
 	"isolation.tools.ghi.launchmenu.change"="true";
@@ -86,10 +88,10 @@ $ExtraOptions = @{
 
 	# Disable VIX Messaging from VM
 	"isolation.tools.vixMessage.disable"="true";
-	
+
 	# Disable logging
 	#"logging"="false";	
-	
+
 	# 5.0 Disable HGFS file transfers [automated VMTools Upgrade]
 	"isolation.tools.hgfsServerSet.disable"="true";
 
@@ -101,7 +103,7 @@ $ExtraOptions = @{
 
 	# 5.0 Do not send host information to guests
 	"tools.guestlib.enableHostInfo"="false";
-	
+
 }   # build our configspec using the hashtable from above.
 
 $vmConfigSpec = New-Object VMware.Vim.VirtualMachineConfigSpec
@@ -116,8 +118,8 @@ Foreach ($Option in $ExtraOptions.GetEnumerator()) {
 # Get all vm's not including templates
 #$VMs = Get-View -ViewType VirtualMachine -Property Name -Filter @{"Config.Template"="false"}   # Do it!
 
-# Only going to get test vm's for now
-$VMs = Get-View -ViewType VirtualMachine -Property Name -Filter @{"name"="test-vm"}
+# Only going to get dev vm's for now
+$VMs = Get-View -ViewType VirtualMachine -Property Name -Filter @{"name"="dev*"}
 
 foreach($VM in $VMs){
     $vm.ReconfigVM($vmConfigSpec)
